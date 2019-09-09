8382e34918329f03d3a80e9cc79fb8f63475095d
S. Hessam M. Mehr <Hessam.Mehr@glasgow.ac.uk>
2019-09-09 18:36:35 +0100

    More work on the ExperimentManager.

--- chem_oracle/experiment.py
+++ chem_oracle/experiment.py
@@ -1,25 +1,41 @@
 import logging
 import random
+from os import path
 from typing import List, Union
 
+import numpy as np
 import pandas as pd
 
 from ms_analyze.ms import MassSpectra
-from .probabilistic_model import StructuralModel, NonstructuralModel
-from chem_oracle.monitoring import parse_filename
+
+from chem_oracle.probabilistic_model import NonstructuralModel, StructuralModel
 
 
-class Experiment:
-    def __init__(self, starting_materials: List[int], ms: MassSpectra = None, nmr=None):
-        self.starting_materials = starting_materials
-        self.ms = ms
-        self.nmr = nmr
+def parse_nmr_filename(filename: str) -> List[str]:
+    folder, filename = path.split(filename)
+    name, ext = path.splitext(filename)
+    parts = name.split("-")
+    return [path.join(folder, part + ".csv") for part in parts]
 
-    def is_reactive(self, max_error: float = 0.2):
-        components = self.ms.find_components_adaptive(
-            max_error=max_error, min_components=len(self.starting_materials)
-        )
-        return len(components) > len(self.starting_materials)
+
+def parse_ms_filename(filename: str) -> List[str]:
+    folder, filename = path.split(filename)
+    name, ext = path.splitext(filename)
+    parts = name.split("-")
+    return [path.join(folder, part + ".csv") for part in parts]
+
+
+def ms_is_reactive(
+    ms: MassSpectra, starting_materials: List[MassSpectra], max_error: float = 0.2
+):
+    components = ms.find_components_adaptive(
+        max_error=max_error, min_components=len(starting_materials)
+    )
+    return len(components) > len(starting_materials)
+
+
+def nmr_is_reactive(*args):
+    pass
 
 
 class ExperimentManager:
@@ -43,6 +59,7 @@ class ExperimentManager:
                 each compound using a structural fingerprint string is used;
                 otherwise they are treated as black boxes. 
         """
+        self.xlsx_file = xlsx_file
         self.nmr_path = nmr_path
         self.ms_path = ms_path
         self.hplc_path = hplc_path
@@ -54,11 +71,25 @@ class ExperimentManager:
         # set up logging
         self.logger = logging.getLogger("experiment-manager")
 
-        with pd.ExcelFile(xlsx_file) as reader:
-            self.reagent_df = pd.read_excel(
-                reader, sheet_name="reagents", dtype=(int, str)
+        self.read_experiments()
+
+        self.n_compounds = len(self.reagents_df["compound"].unique())
+
+        if structural_model:
+            # calculate fingerprints
+            fingerprints = None
+            self.model = StructuralModel(fingerprints, N_props)
+        else:
+            self.model = NonstructuralModel(self.n_compounds, N_props)
+
+    def read_experiments(self):
+        with pd.ExcelFile(self.xlsx_file) as reader:
+            self.reagents_df = pd.read_excel(
+                reader,
+                sheet_name="reagents",
+                dtype=(int, str, str),  # reagent_number  # reagent_name  # data_folder
             )
-            self.reactions_df = pd.read_excel(
+            self.reactions_df: pd.DataFrame = pd.read_excel(
                 reader,
                 sheet_name="reactions",
                 dtype=(
@@ -75,39 +106,45 @@ class ExperimentManager:
                     str,  # data_folder
                 ),
             )
-            self.todo_df = pd.read_excel(reader, sheet_name="todo")
 
-        self.n_compounds = len(self.reagent_df["compound"].unique())
+    def write_experiments(self):
+        with pd.ExcelWriter(self.xlsx_file) as writer:
+            self.reagents_df.to_excel(writer, sheet_name="reagents")
+            self.reactions_df.to_excel(writer, sheet_name="reactions")
 
-        if structural_model:
-            # calculate fingerprints
-            fingerprints = None
-            self.model = StructuralModel(fingerprints, N_props)
-        else:
-            self.model = NonstructuralModel(self.n_compounds, N_props)
-
-    def update(self, n_samples=500, sampler_params=None, write=True):
+    def update(self, n_samples=500, sampler_params=None):
         """Update expected reactivities using probabilistic model.
         
         Args:
             observation (Observation): New reactivity observation to add.
         """
+        # # select reactions with at leasty observation
+        # selector = (
+        #     self.reactions_df["NMR_reactivity"].notna()
+        #     | self.reactions_df["MS_reactivity"].notna()
+        # )
         self.model.condition(self.reactions_df, n_samples, sampler_params or {})
-        # TODO: update dataframe from trace
-        if write:
-            new_xlsx_file = ""
-            self.write(new_xlsx_file)
+        trace = self.model.trace
+        # caculate reactivity for binary reactions
+        bin_avg = 1 - np.mean(trace["bin_doesnt_react"], axis=0)
+        bin_std = np.std(trace["bin_doesnt_react"], axis=0)
+        # caculate reactivity for three component reactions
+        tri_avg = 1 - np.mean(trace["tri_doesnt_react"], axis=0)
+        tri_std = np.std(trace["tri_doesnt_react"], axis=0)
 
-    def write(self, xlsx_file: str):
-        """Write updated experiment state to Excel workbook.
-        
-        Args:
-            xlsx_file (str): Name of Excel workbook to commit results to.
-        """
-        with pd.ExcelWriter(xlsx_file) as writer:
-            self.reagent_df.to_excel(writer, sheet_name="reagents")
-            self.reactions_df.to_excel(writer, sheet_name="reactions")
-            self.todo_df.to_excel(writer, sheet_name="todo")
+        # sync state before writing to avoid data loss
+        self.read_experiments()
+        df = self.reactions_df
+        # update dataframe with calculated reactivities
+        df.loc[
+            df["compound3"] == -1,
+            ["avg_expected_reactivity", "std_expected_reactivity"],
+        ] = np.stack(bin_avg, bin_std)
+        df.loc[
+            df["compound3"] != -1,
+            ["avg_expected_reactivity", "std_expected_reactivity"],
+        ] = np.stack(tri_avg, tri_std)
+        self.write_experiments()
 
     def get_spectrum(self, reagent_number: int):
         if reagent_number == -1:
@@ -116,33 +153,35 @@ class ExperimentManager:
     @property
     def nmr_callback(self):
         def callback(filename):
-            # 1. lookup the reaction (look up the spectra for starting materials and reaction mixture)
-            spectrum = NMRSpectrum(filename)
-            components = parse_filename(filename)
-            try:
-                component_spectra = [
-                    self.get_spectrum(component) for component in components
-                ]
-            except FileNotFoundError as e:
-                self.logger.warn(
-                    f"{e}\nCould not open component spectra; model update aborted."
-                )
-                return
-            reactivity = nmr_reactivity(spectrum, component_spectra)
-            rdf = self.reactions_df
-            rdf.loc[
-                (rdf["compound1"] == components[0])
-                & (rdf["compound2"] == components[1])
-                & (rdf["compound3"] == components[2]),
-                "NMR_reactivity",
-            ] = reactivity
-            self.update()
+            components = parse_nmr_filename(filename)
+            if len(components) > 1:  # reaction mixture — evaluate reactivity
+                reactivity = nmr_is_reactive(filename, components)
+                rdf = self.reactions_df
+                rdf.loc[
+                    (rdf["compound1"] == components[0])
+                    & (rdf["compound2"] == components[1])
+                    & (rdf["compound3"] == components[2]),
+                    "NMR_reactivity",
+                ] = reactivity
+                self.update()
 
         return callback
 
     @property
     def ms_callback(self):
         def callback(filename):
+            components = parse_ms_filename(filename)
+            if len(components) > 1:  # reaction mixture — evaluate reactivity
+                reactivity = ms_is_reactive(filename, components)
+                rdf = self.reactions_df
+                rdf.loc[
+                    (rdf["compound1"] == components[0])
+                    & (rdf["compound2"] == components[1])
+                    & (rdf["compound3"] == components[2]),
+                    "MS_reactivity",
+                ] = reactivity
+                self.write_experiments()
+                self.update()
             pass
 
         return callback
--- chem_oracle/probabilistic_model.py
+++ chem_oracle/probabilistic_model.py
@@ -1,49 +1,101 @@
+import numpy as np
 import pymc3 as pm
 import theano.tensor as tt
 
-from .util import *
+from .util import triangular_tensor, tri_doesnt_react, indices
 
 
 class NonstructuralModel:
-    
     def __init__(self, ncompounds, N=4):
         """fingerprints: Matrix of Morgan fingerprints for reagents."""
         self.N = N
         self.bin_indices = indices(N, 2)
         self.tri_indices = indices(N, 3)
         self.ncompounds = ncompounds
-        
+
     def _pymc3_model(self, facts):
         bin_facts = facts[facts["compound3"] == -1]
         tri_facts = facts[facts["compound3"] != -1]
-        
-        bin_r1, bin_r2 = tt._shared(bin_facts['compound1'].values), tt._shared(bin_facts['compound2'].values)
-        tri_r1, tri_r2, tri_r3 = tt._shared(tri_facts['compound1'].values), tt._shared(tri_facts['compound2'].values), tt._shared(tri_facts['compound3'].values)
+
+        bin_r1, bin_r2 = (
+            tt._shared(bin_facts["compound1"].values),
+            tt._shared(bin_facts["compound2"].values),
+        )
+        tri_r1, tri_r2, tri_r3 = (
+            tt._shared(tri_facts["compound1"].values),
+            tt._shared(tri_facts["compound2"].values),
+            tt._shared(tri_facts["compound3"].values),
+        )
         with pm.Model() as m:
-            mem = pm.Uniform('mem', lower=0.0, upper=1.0, shape=(self.ncompounds,self.N))
-            bin_reactivities = pm.Uniform('bin_reactivities', lower=0.0, upper=1.0, shape=self.N*(self.N-1)//2)
-            tri_reactivities = pm.Uniform('tri_reactivities', lower=0.0, upper=1.0, shape=self.N*(self.N-1)*(self.N-2)//6)
-            react_tensor = pm.Deterministic('react_tensor', triangular_tensor(tri_reactivities,self.N,3,self.tri_indices))
-            react_matrix = pm.Deterministic('react_matrix', triangular_tensor(bin_reactivities,self.N,2,self.bin_indices))
+            mem = pm.Uniform(
+                "mem", lower=0.0, upper=1.0, shape=(self.ncompounds, self.N)
+            )
+            bin_reactivities = pm.Uniform(
+                "bin_reactivities",
+                lower=0.0,
+                upper=1.0,
+                shape=self.N * (self.N - 1) // 2,
+            )
+            tri_reactivities = pm.Uniform(
+                "tri_reactivities",
+                lower=0.0,
+                upper=1.0,
+                shape=self.N * (self.N - 1) * (self.N - 2) // 6,
+            )
+            react_tensor = pm.Deterministic(
+                "react_tensor",
+                triangular_tensor(tri_reactivities, self.N, 3, self.tri_indices),
+            )
+            react_matrix = pm.Deterministic(
+                "react_matrix",
+                triangular_tensor(bin_reactivities, self.N, 2, self.bin_indices),
+            )
             # memberships of binary reactions
-            m1, m2 = mem[bin_r1,:][:,:,np.newaxis], mem[bin_r2,:][:,np.newaxis,:]
-            bin_doesnt_react = pm.Deterministic("bin_doesnt_react", tt.prod(1 - tt.batched_dot(m1, m2) * react_matrix[np.newaxis,:,:], axis=[1,2]))
+            m1, m2 = mem[bin_r1, :][:, :, np.newaxis], mem[bin_r2, :][:, np.newaxis, :]
+            bin_doesnt_react = pm.Deterministic(
+                "bin_doesnt_react",
+                tt.prod(
+                    1 - tt.batched_dot(m1, m2) * react_matrix[np.newaxis, :, :],
+                    axis=[1, 2],
+                ),
+            )
             # memberships of ternary reactions
-            M1, M2, M3 = mem[tri_r1,:], mem[tri_r2,:], mem[tri_r3,:]
-            tri_no_react = pm.Deterministic("tri_doesnt_react", tri_doesnt_react(M1, M2, M3, react_matrix, react_tensor))
-            
-            reacts_obs_binary = pm.Bernoulli('reacts_binary', 1 - bin_doesnt_react, observed=bin_facts['reactivity_label'].values)
-            reacts_obs_ternary = pm.Bernoulli('reacts_ternary', 1 - tri_no_react, observed=tri_facts['reactivity_label'].values)
+            M1, M2, M3 = mem[tri_r1, :], mem[tri_r2, :], mem[tri_r3, :]
+            tri_no_react = pm.Deterministic(
+                "tri_doesnt_react",
+                tri_doesnt_react(M1, M2, M3, react_matrix, react_tensor),
+            )
+
+            nmr_obs_binary = pm.Bernoulli(
+                "reacts_binary",
+                1 - bin_doesnt_react,
+                observed=bin_facts["NMR_reactivity"].values,
+            )
+            ms_obs_binary = pm.Bernoulli(
+                "reacts_binary",
+                1 - bin_doesnt_react,
+                observed=bin_facts["MS_reactivity"].values,
+            )
+            nmr_obs_ternary = pm.Bernoulli(
+                "reacts_ternary",
+                1 - tri_no_react,
+                observed=tri_facts["NMR_reactivity"].values,
+            )
+            ms_obs_ternary = pm.Bernoulli(
+                "reacts_ternary",
+                1 - tri_no_react,
+                observed=tri_facts["MS_reactivity"].values,
+            )
         return m
-    
+
     def condition(self, facts, n_samples, **sampler_params):
         print(f"training on {facts.shape}")
         m = self._pymc3_model(facts)
         with m:
             self.trace = pm.sample(n_samples, **sampler_params)
 
+
 class StructuralModel:
-    
     def __init__(self, fingerprints, N=4):
         """fingerprints: Matrix of Morgan fingerprints for reagents."""
         self.N = N
@@ -51,32 +103,95 @@ class StructuralModel:
         self.tri_indices = indices(N, 3)
         self.fingerprints = tt._shared(fingerprints)
         self.ncompounds, self.fingerprint_length = fingerprints.shape
-        
+
     def _pymc3_model(self, facts):
         bin_facts = facts[facts["compound3"] == -1]
         tri_facts = facts[facts["compound3"] != -1]
-        
-        bin_r1, bin_r2 = tt._shared(bin_facts['compound1'].values), tt._shared(bin_facts['compound2'].values)
-        tri_r1, tri_r2, tri_r3 = tt._shared(tri_facts['compound1'].values), tt._shared(tri_facts['compound2'].values), tt._shared(tri_facts['compound3'].values)
+
+        bin_r1, bin_r2 = (
+            tt._shared(bin_facts["compound1"].values),
+            tt._shared(bin_facts["compound2"].values),
+        )
+        tri_r1, tri_r2, tri_r3 = (
+            tt._shared(tri_facts["compound1"].values),
+            tt._shared(tri_facts["compound2"].values),
+            tt._shared(tri_facts["compound3"].values),
+        )
         with pm.Model() as m:
-            mem = pm.Uniform('mem', lower=0.0, upper=1.0, shape=(self.fingerprint_length,self.N))
-            bin_reactivities = pm.Uniform('bin_reactivities', lower=0.0, upper=1.0, shape=self.N*(self.N-1)//2)
-            tri_reactivities = pm.Uniform('tri_reactivities', lower=0.0, upper=1.0, shape=self.N*(self.N-1)*(self.N-2)//6)
-            react_tensor = pm.Deterministic('react_tensor', triangular_tensor(tri_reactivities,self.N,3,self.tri_indices))
-            react_matrix = pm.Deterministic('react_matrix', triangular_tensor(bin_reactivities,self.N,2,self.bin_indices))
+            mem = pm.Uniform(
+                "mem", lower=0.0, upper=1.0, shape=(self.fingerprint_length, self.N)
+            )
+            bin_reactivities = pm.Uniform(
+                "bin_reactivities",
+                lower=0.0,
+                upper=1.0,
+                shape=self.N * (self.N - 1) // 2,
+            )
+            tri_reactivities = pm.Uniform(
+                "tri_reactivities",
+                lower=0.0,
+                upper=1.0,
+                shape=self.N * (self.N - 1) * (self.N - 2) // 6,
+            )
+            react_tensor = pm.Deterministic(
+                "react_tensor",
+                triangular_tensor(tri_reactivities, self.N, 3, self.tri_indices),
+            )
+            react_matrix = pm.Deterministic(
+                "react_matrix",
+                triangular_tensor(bin_reactivities, self.N, 2, self.bin_indices),
+            )
             # memberships of binary reactions
             fp1, fp2 = self.fingerprints[bin_r1, :], self.fingerprints[bin_r2, :]
-            m1, m2 = tt.max(tt.mul(fp1[:, :, np.newaxis], mem), axis=1)[:,:,np.newaxis], tt.max(tt.mul(fp2[:, :, np.newaxis], mem), axis=1)[:,np.newaxis,:]
-            bin_doesnt_react = pm.Deterministic("bin_doesnt_react", tt.prod(1 - tt.batched_dot(m1, m2) * react_matrix[np.newaxis,:,:], axis=[1,2]))
+            m1, m2 = (
+                tt.max(tt.mul(fp1[:, :, np.newaxis], mem), axis=1)[:, :, np.newaxis],
+                tt.max(tt.mul(fp2[:, :, np.newaxis], mem), axis=1)[:, np.newaxis, :],
+            )
+            bin_doesnt_react = pm.Deterministic(
+                "bin_doesnt_react",
+                tt.prod(
+                    1 - tt.batched_dot(m1, m2) * react_matrix[np.newaxis, :, :],
+                    axis=[1, 2],
+                ),
+            )
             # memberships of ternary reactions
-            FP1, FP2, FP3 = self.fingerprints[tri_r1, :], self.fingerprints[tri_r2, :], self.fingerprints[tri_r3, :]
-            M1, M2, M3 = tt.max(tt.mul(FP1[:, :, np.newaxis], mem), axis=1), tt.max(tt.mul(FP2[:, :, np.newaxis], mem), axis=1), tt.max(tt.mul(FP3[:, :, np.newaxis], mem), axis=1)
-            tri_no_react = pm.Deterministic("tri_doesnt_react", tri_doesnt_react(M1, M2, M3, react_matrix, react_tensor))
-            
-            reacts_obs_binary = pm.Bernoulli('reacts_binary', 1 - bin_doesnt_react, observed=bin_facts['reactivity_label'].values)
-            reacts_obs_ternary = pm.Bernoulli('reacts_ternary', 1 - tri_no_react, observed=tri_facts['reactivity_label'].values)
+            FP1, FP2, FP3 = (
+                self.fingerprints[tri_r1, :],
+                self.fingerprints[tri_r2, :],
+                self.fingerprints[tri_r3, :],
+            )
+            M1, M2, M3 = (
+                tt.max(tt.mul(FP1[:, :, np.newaxis], mem), axis=1),
+                tt.max(tt.mul(FP2[:, :, np.newaxis], mem), axis=1),
+                tt.max(tt.mul(FP3[:, :, np.newaxis], mem), axis=1),
+            )
+            tri_no_react = pm.Deterministic(
+                "tri_doesnt_react",
+                tri_doesnt_react(M1, M2, M3, react_matrix, react_tensor),
+            )
+
+            nmr_obs_binary = pm.Bernoulli(
+                "reacts_binary",
+                1 - bin_doesnt_react,
+                observed=bin_facts["NMR_reactivity"].values,
+            )
+            ms_obs_binary = pm.Bernoulli(
+                "reacts_binary",
+                1 - bin_doesnt_react,
+                observed=bin_facts["MS_reactivity"].values,
+            )
+            nmr_obs_ternary = pm.Bernoulli(
+                "reacts_ternary",
+                1 - tri_no_react,
+                observed=tri_facts["NMR_reactivity"].values,
+            )
+            ms_obs_ternary = pm.Bernoulli(
+                "reacts_ternary",
+                1 - tri_no_react,
+                observed=tri_facts["MS_reactivity"].values,
+            )
         return m
-    
+
     def condition(self, facts, n_samples, **sampler_params):
         print(f"training on {facts.shape}")
         m = self._pymc3_model(facts)
