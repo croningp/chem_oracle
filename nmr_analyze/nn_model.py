368eab27f3abf15f5c9888f737506ffcf29f4a34
DMC <d.caramelli.1@research.gla.ac.uk>
2019-09-23 15:02:18 +0100

    remove code for training, clean function to read dat

--- nmr_analyze/nn_model.py
+++ nmr_analyze/nn_model.py
@@ -149,152 +149,6 @@ class NMR_nn:
 
         return logits, y_pred
 
-    def train(self, trsetx, trsety, valsetx, valsety):
-
-        tf.reset_default_graph()
-
-        x = tf.placeholder(tf.float32, shape=[None, 2, self.spectrum_shape])
-        phase = tf.placeholder(tf.bool)
-        y_true = tf.placeholder(tf.float32, shape=[None, 4])
-
-        logits, y_pred = self.build_model(x, phase)
-
-        cross_entropy = -(
-            y_true * tf.log(y_pred + 1e-12) + (1 - y_true) * tf.log(1 - y_pred + 1e-12)
-        )
-        loss = tf.reduce_mean(cross_entropy)
-
-        # Define global step and decaying learning rate
-        global_step = tf.Variable(0, trainable=False)
-        learning_rate = tf.train.exponential_decay(
-            learning_rate=self.initial_learning_rate,
-            global_step=global_step,
-            decay_steps=300,
-            decay_rate=0.95,
-            staircase=False,
-        )
-        # Define optimization step
-        optimize = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(
-            loss, global_step=global_step
-        )
-
-        sess = tf.Session()
-        saver = tf.train.Saver()
-        sess.run(tf.global_variables_initializer())
-        training_data_manager = DataManager(trsetx, trsety, random_shift=True)
-        iter_per_epoch = math.ceil(len(trsetx) / self.batch_size)
-
-        best_val_loss = np.inf
-        val_losses = []
-        training_losses = []
-        epochs = []
-
-        for e in range(self.num_epochs):
-            e_losses = []
-            for i in range(iter_per_epoch):
-                batchx, batchy = training_data_manager.next_batch(self.batch_size)
-                # batchy = batchy.reshape([-1, 1])
-                _, iloss = sess.run(
-                    [optimize, loss], feed_dict={x: batchx, y_true: batchy, phase: True}
-                )
-                #                 print ('loss:\t ', iloss)
-                e_losses.append(iloss)
-            print("Epoch {} training loss {}".format(e, np.mean(e_losses)))
-
-            val_loss = sess.run(
-                loss, feed_dict={x: valsetx, y_true: valsety, phase: False}
-            )
-
-            val_losses.append(val_loss)
-            training_losses.append(np.mean(e_losses))
-            epochs.append(e)
-
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
-                print("New validation loss found {}".format(val_loss))
-                saver.save(sess, self.model_path)
-
-    def test(self, testx, testy):
-        # Reset tensorflow graph
-        tf.reset_default_graph()
-
-        # Placeholders for feeding the data
-        x = tf.placeholder(tf.float32, shape=[None, 2, self.spectrum_shape])
-        phase = tf.placeholder(tf.bool)
-        y_true = tf.placeholder(tf.float32, shape=[None, 4])
-
-        # Build the model and get logits and predicted reactivty classes
-        logits, y_pred = self.build_model(x, phase)
-        # Define crossentropy loss here
-        cross_entropy = -(
-            y_true * tf.log(y_pred + 1e-12) + (1 - y_true) * tf.log(1 - y_pred + 1e-12)
-        )
-        loss = tf.reduce_mean(cross_entropy)
-
-        top2_accuracy = tf.reduce_mean(
-            tf.cast(
-                tf.nn.in_top_k(
-                    predictions=y_pred, targets=tf.argmax(y_true, axis=-1), k=2
-                ),
-                tf.float32,
-            )
-        )
-        accuracy = tf.reduce_mean(
-            tf.cast(
-                tf.nn.in_top_k(
-                    predictions=y_pred, targets=tf.argmax(y_true, axis=-1), k=1
-                ),
-                tf.float32,
-            )
-        )
-
-        sess = tf.Session()
-        saver = tf.train.Saver()
-        sess.run(tf.global_variables_initializer())
-        saver.restore(sess, self.model_path)
-
-        feed_dict = {x: testx, y_true: testy, phase: False}
-        test_loss, test_pred, test_accuracy, test_top2_accuracy = sess.run(
-            [loss, y_pred, accuracy, top2_accuracy], feed_dict=feed_dict
-        )
-        reactivity = np.argmax(test_pred, axis=1)
-
-        print("--------------------------Test accuracy {}".format(test_accuracy))
-        print("Top 2 accuracy {}".format(test_top2_accuracy))
-
-        true_reactivity = np.argmax(testy, axis=1)
-        hits = np.equal(reactivity, true_reactivity)
-        accuracy = np.mean(hits.astype(np.float32))
-
-        results = [[i, j] for i, j in zip(reactivity, true_reactivity)]
-
-        distr = [[], [], [], []]
-
-        result = 0
-
-        for exp in results:
-            distr[exp[1]].append(exp)
-        for i in range(len(distr)):
-            count = 0
-            clas = distr[i]
-            for exp in clas:
-                if exp[1] == exp[0]:
-                    count += 1
-            print("{} - accuracy of {}%".format(i, count / len(clas)))
-        result += count / len(clas)
-        m = np.zeros([4, 4])
-        for i in results:
-            m[i[0], i[1]] += 1
-        # %matplotlib qt
-        plt.figure()
-        plt.imshow(m)
-        #         plt.title(param)
-        plt.xlabel("True", fontsize=15)
-        plt.ylabel("Predicted", fontsize=15)
-        for y in range(4):
-            for x in range(4):
-                plt.annotate(int(m[y, x]), (x - 0.2, y + 0.1), fontsize=17)
-        print(reactivity)
 
     def predict(self, datax):
         # Reset tensorflow graph
@@ -314,7 +168,6 @@ class NMR_nn:
 
         feed_dict = {x: datax, phase: False}
         prediction = sess.run([y_pred], feed_dict=feed_dict)
-        reactivity = np.argmax(prediction, axis=1)
         return prediction
 
 
@@ -322,12 +175,11 @@ def full_nmr_process(file_path, reagents, reagent_folder):
     """
 
     :param file_path: folder of raw nmr data
-    :param reagents: list of reagent names, the reagents folder is currently hardcoded inside the get_theoretical_nmr
-    in utils.
+    :param reagents: list of reagent names, these are the folder names in reagent_folder
+    :param reagent_folder: the folder cotaining the nmr of the reagents.
     :return: value of rectivity between 0 and 1
     """
-    data = raw_nmr_to_dataframe(file_path, reagents, reagent_folder)
-    data_x, data_y = read_data(data)
+    data_x = make_input_matrix(file_path, reagents, reagent_folder)
     nn = NMR_nn()
     reactivity = nn.predict(data_x)
     weighted_reactivity = reactivity * (np.array([0, 1, 2, 3]))
