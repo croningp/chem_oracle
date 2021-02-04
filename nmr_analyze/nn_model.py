bcd340acc4b301f1328019ed709a3c7f6d6e118f
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2021-02-04 16:55:25 +0000

    Restrict GPU memory usage of NMR model.

--- nmr_analyze/nn_model.py
+++ nmr_analyze/nn_model.py
@@ -10,16 +10,19 @@ from nmr_analyze.nmr_analysis import NMRDataset, NMRSpectrum
 
 DEFAULT_XFORM = lambda s: s.crop(0, 12, inplace=True).normalize(inplace=True)
 
+
 gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
     try:
-        # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=256)],
+        )
+        logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
+        # Virtual devices must be set before GPUs have been initialized
         print(e)
 
 
